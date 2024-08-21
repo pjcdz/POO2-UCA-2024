08a7d84bfb590177a467fa73d36e000a2c616b1f
Pablo Cardozo <85005529+pjcdz@users.noreply.github.com>
2024-08-21 20:02:09 -0300

    Refactor pagarViaje function to remove if statement

---
+++ tests/c21-08-24/TarjetaSube.js
@@ -0,0 +1,25 @@
+class TarjetaSube {
+    constructor(id) {
+        this.id = id;
+        this.saldo = 0;
+        this.saldoMinimo = -600;
+    }
+
+    obtenerSaldo() {
+        return this.saldo;
+    }
+
+    cargarSaldo(saldoACargar) {
+        return this.saldo = this.saldo + saldoACargar;
+    }
+
+    pagarViaje(precioViaje) {
+        if ( !((this.saldo - precioViaje) >= this.saldoMinimo) ) {
+            throw new Error("Saldo insuficiente.");
+        } 
+
+        return this.saldo = this.saldo - precioViaje;
+    }
+}
+
+module.exports = TarjetaSube;
