d64b65708bd80e754012c671e6edc403846b9455
Pablo Cardozo <85005529+pjcdz@users.noreply.github.com>
2024-09-15 23:52:02 -0300

    romano

--- tests/c09-11-24/numeroRomano.js
+++ tests/c09-11-24/numeroRomano.js
@@ -1,8 +1,36 @@
-function numeroRomano(num) {
-    let num5 = Math.floor(num / 5);
-    let num1 = num % 5;
+function numeroRomano(numero) {
+    // let num5 = Math.floor(num / 5);
+    // let num1 = num % 5;
     
-    return 'V'.repeat(num5) + 'I'.repeat(num1);
-}   
+    // return 'V'.repeat(num5) + 'I'.repeat(num1);
+
+    let romano = '';
+
+    // Definir los valores romanos y sus equivalentes numéricos
+    const valoresRomanos = [
+        { valor: 10, simbolo: 'X' },
+        { valor: 9, simbolo: 'IX' },
+        { valor: 5, simbolo: 'V' },
+        { valor: 4, simbolo: 'IV' },
+        { valor: 1, simbolo: 'I' }
+    ];
+
+    // Iterar sobre los valores romanos
+    for (let i = 0; i < valoresRomanos.length; i++) {
+        let valorActual = valoresRomanos[i].valor;
+        let simboloActual = valoresRomanos[i].simbolo;
+
+        // Calcular cuántas veces el número puede ser dividido por el valor actual
+        let cociente = Math.floor(numero / valorActual);
+
+        // Añadir el símbolo romano correspondiente tantas veces como sea necesario
+        romano += simboloActual.repeat(cociente);
+
+        // Reducir el valor del número
+        numero %= valorActual;
+    }
+
+    return romano;
+}
 
 module.exports = numeroRomano;
--- tests/c09-11-24/numeroRomano.test.js
+++ tests/c09-11-24/numeroRomano.test.js
@@ -1,25 +1,18 @@
 const numeroRomano = require('./numeroRomano.js');
 
-test('Conversion of 1 to Roman numeral should be "I"', () => {
-    expect(numeroRomano(1)).toBe("I");
+test('Conversion of 7 to Roman numeral should be "VII"', () => {
+    expect(numeroRomano(7)).toBe("VII");
 });
 
-test('Conversion of 6 to Roman numeral should be "VI"', () => {
-    expect(numeroRomano(6)).toBe("VI");
+test('Conversion of 9 to Roman numeral should be "IX"', () => {
+    expect(numeroRomano(9)).toBe("IX");
 });
 
-// test('Conversion of 9 to Roman numeral should be "IX"', () => {
-//     expect(numeroRomano(9)).toBe("IX");
-// });
+test('Conversion of 14 to Roman numeral should be "XIV"', () => {
+    expect(numeroRomano(14)).toBe("XIV");
+});
 
-// test('Conversion of 40 to Roman numeral should be "XL"', () => {
-//     expect(numeroRomano(40)).toBe("XL");
-// });
+test('Conversion of 5 to Roman numeral should be "V"', () => {
+    expect(numeroRomano(5)).toBe("V");
+});
 
-// test('Conversion of 90 to Roman numeral should be "XC"', () => {
-//     expect(numeroRomano(90)).toBe("XC");
-// });
-
-// test('Conversion of 3999 to Roman numeral should be "MMMCMXCIX"', () => {
-//     expect(numeroRomano(3999)).toBe("MMMCMXCIX");
-// });
